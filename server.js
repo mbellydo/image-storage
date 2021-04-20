d3807387a2b83613b00a7917acc9668a4660b747
marc <mbellydo@gmail.com>
2021-04-20 12:16:13 +0200

    done

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 // OUR "BBDD" 
 var images = [
-    { title: 'Perro_gordo', url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.W4A7fToVwIlIaJMcD7_inAHaFj%26pid%3DApi&f=1", date: "2018-01-17"},
-    { title: 'Test_fecha', url: "https://i.picsum.photos/id/722/200/300.jpg?hmac=MDrZtULoytyxS357HVHCqzJRUv_BsxU0MEgszPVuMyY", date: "2021-01-17"}
+    { title: 'Perro_gordo', url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.W4A7fToVwIlIaJMcD7_inAHaFj%26pid%3DApi&f=1", date: "2018-01-17", color: "rgb(113, 90, 81)"},
+    { title: 'Test_fecha', url: "https://i.picsum.photos/id/722/200/300.jpg?hmac=MDrZtULoytyxS357HVHCqzJRUv_BsxU0MEgszPVuMyY", date: "2021-01-17", color:"rgb(205, 188, 152)"}
 ];
 // load the things we need
 var express = require('express');
 var app = express();
-const ColorThief = require('colorthief');
-/*var color = require('dominant-color');
-var imgPath = images.url;
-
-color(imgPath, {format: 'rgb'}, function (err, color) {
-    console.log(color)
-})*/
+const { getColorFromURL, getPaletteFromURL } = require('color-thief-node');
+const chalk = require('chalk');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -25,13 +20,13 @@ app.use(express.urlencoded({extended: false}))
 app.get('/', function(req, res) {
 
     var tagline = "These are the images you uploaded";
-
+    console.log(images)
     res.render('pages/index', {
         images: images.sort(function (a,b) {
             var dateA = new Date(a.date), dateB = new Date(b.date)
 	        return dateB - dateA
         }),
-        tagline: tagline
+        tagline: tagline,
     });
 });
 
@@ -53,11 +48,22 @@ function checkImgExists(link){
     return imageFound
 }
 
-app.post('/add-img', function(req, res) {
+async function getColor(url) {
+    const dominantColor = await getColorFromURL(url);
+    //const [r, g, b] = dominantColor;
+    const color = `rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`
+    console.log(
+        'Dominant Color:',
+        color
+    );
+    return color
+}
+
+app.post('/add-img', async function(req, res) {
     const title = req.body.title;
     const url = req.body.url;
     const date = req.body.date;
-    
+
     if ( title.length > 0){
         if ( title.match('^.*$') ) {
             console.log('Title is valid')
@@ -70,10 +76,14 @@ app.post('/add-img', function(req, res) {
         res.send("Esta Imagen ya esta almacenada, no se añadirá")
     } else {
         console.log("url no repetida")
+        
+        const color = await getColor(url)
+        
         images.push({
             title: title,
             url: url,
-            date: date
+            date: date,
+            color: color,
         });
 
         res.redirect('/')
