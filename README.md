7fc2cd018f100bf2f43be2e002ca94debbf12abd
marc <mbellydo@gmail.com>
2021-04-20 08:42:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Image storage server
+
+This webpage claims to be a little back end practice to storage url images.
---
+++ server.js
@@ -0,0 +1,85 @@
+// OUR "BBDD" 
+var images = [
+    { title: 'Perro_gordo', url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.W4A7fToVwIlIaJMcD7_inAHaFj%26pid%3DApi&f=1", date: "2018-01-17"},
+    { title: 'Test_fecha', url: "https://i.picsum.photos/id/722/200/300.jpg?hmac=MDrZtULoytyxS357HVHCqzJRUv_BsxU0MEgszPVuMyY", date: "2021-01-17"}
+];
+// load the things we need
+var express = require('express');
+var app = express();
+const ColorThief = require('colorthief');
+/*var color = require('dominant-color');
+var imgPath = images.url;
+
+color(imgPath, {format: 'rgb'}, function (err, color) {
+    console.log(color)
+})*/
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+// parse url-encoded params
+app.use(express.urlencoded({extended: false}))
+
+// show img page 
+app.get('/', function(req, res) {
+
+    var tagline = "These are the images you uploaded";
+
+    res.render('pages/index', {
+        images: images.sort(function (a,b) {
+            var dateA = new Date(a.date), dateB = new Date(b.date)
+	        return dateB - dateA
+        }),
+        tagline: tagline
+    });
+});
+
+// add img page
+app.get('/add-img', function(req, res) {
+    
+    //console.log(images)
+    res.render('pages/add-img');
+});
+
+function checkImgExists(link){
+    let imageFound = false
+    let i = 0
+    while (i != images.length && !imageFound) {
+        imageFound = images[i].url == link
+        i++
+    }
+
+    return imageFound
+}
+
+app.post('/add-img', function(req, res) {
+    const title = req.body.title;
+    const url = req.body.url;
+    const date = req.body.date;
+    
+    if ( title.length > 0){
+        if ( title.match('^.*$') ) {
+            console.log('Title is valid')
+        } else{
+            console.log('Title is invalid')
+        }
+    }
+
+    if (checkImgExists(url)) {
+        res.send("Esta Imagen ya esta almacenada, no se añadirá")
+    } else {
+        console.log("url no repetida")
+        images.push({
+            title: title,
+            url: url,
+            date: date
+        });
+
+        res.redirect('/')
+    }
+
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
